6b3d035d07bfdec5676673dcdf6b41f87f44b993
erobert <etiennerobert33@gmail.com>
2021-05-02 23:00:32 +0200

    Updates

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('Hello World')
+import React from "./_snowpack/pkg/react.js";
+import ReactDOM from "./_snowpack/pkg/react-dom.js";
+console.log("Hello World");
+const domContainer = document.querySelector("#root");
+ReactDOM.render(/* @__PURE__ */ React.createElement("button", null, "Button"), domContainer);
